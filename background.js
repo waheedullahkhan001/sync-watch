1ed480c6fe3bb24a5af38d19438dcfd2f8ed08e0
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2024-02-22 15:13:31 +0500

    rearranged code

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['content.js']
-    }).then().catch(console.error);
-});
+function setUpMessaging() {
+    chrome.runtime.onMessage.addListener(
+        (request, sender, sendResponse) => {
+            // TODO: Handle messages
+        }
+    );
+}
+
+function setUpActionListener() {
+    chrome.action.onClicked.addListener((tab) => {
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            files: ['content.js']
+        }).then().catch(console.error);
+    });
+}
+
+function main() {
+    setUpActionListener();
+    setUpMessaging();
+}
+
+main();
